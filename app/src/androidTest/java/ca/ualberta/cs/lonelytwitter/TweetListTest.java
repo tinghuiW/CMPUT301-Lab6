47266604e0e5d922f326352e5b3198a2fcb36d57
tinghuiW <tinghui@ualberta.ca>
2017-10-12 18:28:10 -0600

    lab6

---
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -0,0 +1,44 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by tinghui on 10/12/17.
+ */
+
+public class TweetListTest extends ActivityInstrumentationTestCase2 {
+    public TweetListTest(){
+        super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
+    }
+
+    public void testAddTweet(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("adding Tweet");
+        tweets.add(tweet);
+        assertTrue(tweets.hasTweet(tweet));
+    }
+
+    public void testDelete(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("test");
+        tweets.add(tweet);
+        tweets.delete(tweet);
+        assertFalse(tweets.hasTweet(tweet));
+    }
+
+    public void testHasTweet(){
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("test");
+        list.add(tweet);
+        assertTrue(list.hasTweet(tweet));
+    }
+
+    public void testGetTweet() {
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("test");
+        list.add(tweet);
+        Tweet returnedTweet = list.getTweet(0);
+        assertEquals(returnedTweet.getMessage(), tweet.getMessage());
+    }
+
+}
